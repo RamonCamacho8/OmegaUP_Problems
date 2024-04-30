9fc062b388fa30aee15a30215c9307892d8a3ad2
Alberto <118371477+RamonCamacho8@users.noreply.github.com>
2024-04-30 19:05:36 +0000

    LeyendoVariosDatos

--- Basic/LeyendoVariosDatos/Main.java
+++ Basic/LeyendoVariosDatos/Main.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int size = s.nextInt();
+        int size = Integer.parseInt(s.nextLine());
 
-
+        for (int i = 0; i < size; i++) {
+            String[] pair = s.nextLine().split(" ");
+            System.out.println(Integer.parseInt(pair[0]) +  Integer.parseInt(pair[1]));
+        }
+        s.close();
     }
 }
