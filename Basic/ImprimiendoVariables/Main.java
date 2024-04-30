a511edadd36025cf11cc3ba64d3472ec6e49645d
Alberto <118371477+RamonCamacho8@users.noreply.github.com>
2024-04-30 17:47:17 +0000

    ImprimiendoVariables

---
+++ Basic/ImprimiendoVariables/Main.java
@@ -0,0 +1,8 @@
+package Basic.ImprimiendoVariables;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(3.1416);
+    }
+}
